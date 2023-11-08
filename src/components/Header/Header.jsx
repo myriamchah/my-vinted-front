54e388cade914a5141d8512856f1fce3a491ef3b
myriam chahlafi <myriam.chah@gmail.com>
2023-11-08 19:05:01 +0100

    Fix input search and Home css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import "./Header.css";
 import logo from "../../assets/img/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   return (
     <header>
       <div className="container">
         <img src={logo} alt="logo Vinted" />
-        <div className="input-search"></div>
+        <div className="input-search">
+          <input type="text" placeholder="Recherche des articles" />
+          <FontAwesomeIcon icon="magnifying-glass" className="icon" />
+        </div>
+
         <div>
           <button>S'inscrire</button>
           <button>Se connecter</button>
