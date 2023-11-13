722cdbb12ed0af0832aec6328d56bd9062604ec2
myriam chahlafi <myriam.chah@gmail.com>
2023-11-13 15:52:20 +0100

    Add style to form

--- src/pages/Publish.jsx
+++ src/pages/Publish.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
 
+import "../assets/styles/publish.css";
+
 const Publish = () => {
   const [newOffer, setNewOffer] = useState({
     title: "",
@@ -16,27 +18,34 @@ const Publish = () => {
   });
 
   const onChange = (e) => {
-    setNewOffer((prevState) => ({
-      ...prevState,
+    setNewOffer((newOffer) => ({
+      ...newOffer,
       [e.target.name]: e.target.value,
     }));
   };
 
   const onSubmit = async (e) => {
-    e.preventDefault();
     try {
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append("title", newOffer.title);
+      formData.append("description", newOffer.description);
+      formData.append("brand", newOffer.brand);
+      formData.append("size", newOffer.size);
+      formData.append("color", newOffer.color);
+      formData.append("condition", newOffer.condition);
+      formData.append("city", newOffer.city);
+      formData.append("price", newOffer.price);
+      formData.append("picture", newOffer.picture);
+      formData.append("acceptExchange", newOffer.acceptExchange);
+
       const response = await axios.post(
         "https://lereacteur-vinted-api.herokuapp.com/offer/publish",
+        formData,
         {
-          title: newOffer.title,
-          description: newOffer.description,
-          price: newOffer.price,
-          condition: newOffer.condition,
-          city: newOffer.city,
-          brand: newOffer.brand,
-          size: newOffer.size,
-          color: newOffer.color,
-          picture: newOffer.picture,
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         }
       );
       if (response.data._id) {
@@ -55,73 +64,120 @@ const Publish = () => {
         <section className="container publish-container">
           <h1>Vends ton article</h1>
           <form onSubmit={onSubmit} className="publish-form">
-            <label htmlFor="title">Titre</label>
-            <input
-              type="text"
-              name="title"
-              placeholder="ex : chemisier bleu"
-              value={newOffer.title}
-              onChange={onChange}
-            />
-            <label htmlFor="description">Description</label>
-            <input
-              type="text"
-              placeholder="ex : porté plusieurs fois, taille correctement"
-              name="description"
-              value={newOffer.description}
-              onChange={onChange}
-            />
-            <label htmlFor="brand">Marque</label>
-            <input
-              type="text"
-              placeholder="ex : Sezane"
-              name="brand"
-              value={newOffer.brand}
-              onChange={onChange}
-            />
-            <label htmlFor="size">Taille</label>
-            <input
-              type="text"
-              placeholder="ex : M / 38 "
-              name="size"
-              value={newOffer.size}
-              onChange={onChange}
-            />
-            <label htmlFor="color">Couleur</label>
-            <input
-              type="text"
-              placeholder="ex : Bleu "
-              name="color"
-              value={newOffer.color}
-              onChange={onChange}
-            />
-            <label htmlFor="condition">Condition</label>
-            <input
-              type="text"
-              placeholder="ex : Bon état "
-              name="condition"
-              value={newOffer.condition}
-              onChange={onChange}
-            />
-            <label htmlFor="city">Lieu</label>
-            <input
-              type="text"
-              placeholder="ex : Paris "
-              name="city"
-              value={newOffer.city}
-              onChange={onChange}
-            />
-            <label htmlFor="price">Prix</label>
-            <input
-              type="text"
-              placeholder="ex : 0,00 € "
-              name="price"
-              value={newOffer.price}
-              onChange={onChange}
-            />
-
+            <div className="form-card">
+              <input
+                id="file"
+                type="file"
+                className="input-file"
+                onChange={(e) => {
+                  setNewOffer({
+                    ...newOffer,
+                    [newOffer.picture]: e.target.files[0],
+                  });
+                }}
+              />
+              <label htmlFor="file"> + Ajoute une photo</label>
+            </div>
+            <div className="form-card">
+              <div className="form-group">
+                <label htmlFor="title">Titre</label>
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="ex : chemisier bleu"
+                  value={newOffer.title}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="description">Description</label>
+                <input
+                  type="textarea"
+                  placeholder="ex : porté plusieurs fois, taille correctement"
+                  name="description"
+                  value={newOffer.description}
+                  onChange={onChange}
+                  rows={5}
+                />
+              </div>
+            </div>
+            <div className="form-card">
+              <div className="form-group">
+                <label htmlFor="brand">Marque</label>
+                <input
+                  type="text"
+                  placeholder="ex : Sezane"
+                  name="brand"
+                  value={newOffer.brand}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="size">Taille</label>
+                <input
+                  type="text"
+                  placeholder="ex : M / 38 "
+                  name="size"
+                  value={newOffer.size}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="color">Couleur</label>
+                <input
+                  type="text"
+                  placeholder="ex : Bleu "
+                  name="color"
+                  value={newOffer.color}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="condition">Condition</label>
+                <input
+                  type="text"
+                  placeholder="ex : Bon état "
+                  name="condition"
+                  value={newOffer.condition}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="city">Lieu</label>
+                <input
+                  type="text"
+                  placeholder="ex : Paris "
+                  name="city"
+                  value={newOffer.city}
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+            <div className="form-card">
+              <div className="form-group">
+                <label htmlFor="price">Prix</label>
+                <input
+                  type="text"
+                  placeholder="ex : 0,00 € "
+                  name="price"
+                  value={newOffer.price}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-checkbox">
+                <input
+                  type="checkbox"
+                  name="acceptExchange"
+                  value={newOffer.acceptExchange}
+                  onChange={onChange}
+                />
+                <label htmlFor="acceptExchange">
+                  Je suis intéressé(e) par les échanges
+                </label>
+              </div>
+            </div>
             <button className="teal" type="submit">
-              Publier mon offre
+              Publier
             </button>
           </form>
         </section>
